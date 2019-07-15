9fbe1c612c76b098725da2fe73ebae14874a0149
Laurent <l.perroteau@sbt-human.com>
2019-07-15 17:05:45 +0200

    Fetcher datas.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import datas from './datas.json';
+// import datas from './datas.json';
 import Game from './components/Game';
-import PropTypes from 'prop-types';
 import Button from './components/Button';
 
 class App extends Component {
@@ -10,29 +9,49 @@ class App extends Component {
     super(props);
 
     this.state = {
+      datas        : null,
       theme        : null,
       indexQuestion: 0
     }
   }
 
+  async componentDidMount() {
+    try {
+      // Celui dans public
+      const datas = await fetch('datas.json').then(res => res.json());
+      this.setState({
+        datas
+      });
+    } catch (e) {
+      console.error('Erreur au fetch datas.json', e);
+    }
+
+  }
+
   render() {
     return (
       <>
-        {!this.state.theme && <div className="mdl-grid">
-          <div className="mdl-cell mdl-cell--4-col">
-            <h3>Choisiser un thème :</h3>
-          </div>
-          <div className="mdl-cell mdl-cell--4-col">
-            <ul className="mdl-list">
-              {datas.themes.map(theme => (
-                <li className="mdl-list__item" key={theme.value}>
-                  <Button label={theme.name} onClick={() => this._handleChooseTheme(theme.value)} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>}
-        {this.state.theme && <Game questions={datas.questions[this.state.theme]} onEnd={() => this._handleQuestionsEnd()} />}
+        {this.state.datas ?
+          <>
+            {!this.state.theme && <div className="mdl-grid">
+              <div className="mdl-cell mdl-cell--4-col">
+                <h3>Choisiser un thème :</h3>
+              </div>
+              <div className="mdl-cell mdl-cell--4-col">
+                <ul className="mdl-list">
+                  {this.state.datas.themes.map(theme => (
+                    <li className="mdl-list__item" key={theme.value}>
+                      <Button label={theme.name} onClick={() => this._handleChooseTheme(theme.value)} />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>}
+            {this.state.theme && <Game questions={this.state.datas.questions[this.state.theme]} onEnd={() => this._handleQuestionsEnd()} />}
+          </>
+          :
+          <p>loading...</p>
+        }
       </>
     );
   }
